e9b548c440e807da8f3827802e32d45d005e4191
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-05-04 23:16:39 -0400

    Add task2936.java

---
+++ src/task2936.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class task2936 {
+public static void main(String[] args){
+
+    Scanner scanner = new Scanner(System.in);
+
+
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+
+    double c = Math.sqrt(a * a + b * b);
+
+    System.out.print(c);
+
+
+
+
+
+    //Задача №2936. Гипотенуза
+    //Дано два числа a и b. Найдите гипотенузу треугольника с заданными катетами.
+    //
+    //Входные данные
+    //В двух строках вводятся два числа (числа целые,положительные, не превышают 1000).
+    //
+    //Выходные данные
+    //Выведите ответ на задачу.
+
+
+scanner.close();
+}
+
+}
