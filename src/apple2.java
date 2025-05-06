63440e3428677e1df13dd9778edd0435b81adadc
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-05-05 21:10:43 -0400

    Add apple2 task.java

---
+++ src/apple2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class apple2 {
+    public static void main(String[] args){
+
+        /*
+        Задача №2939. Дележ яблок - 2
+N школьников делят K яблок поровну, неделящийся остаток остается в корзинке. Сколько яблок останется в корзинке?
+
+Входные данные
+Программа получает на вход числа N и K.
+
+Выходные данные
+Программа должна вывести искомое количество яблок.
+
+Примеры
+Входные данные
+3
+14
+Выходные данные
+2
+         */
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+
+
+        int rest = K % N;
+
+
+        System.out.println(rest);
+
+scanner.close();
+    }
+}
