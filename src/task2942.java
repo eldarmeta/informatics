8aa8b0ff558f0735064cc28c6929298b72cf74eb
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-05-06 01:11:45 -0400

    Add task2942.java

---
+++ src/task2942.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class task2942 {
+    public static void main(String [] args){
+        /*
+        Задача №2942. Число десятков двузначного числа
+Дано двузначное число. Найдите число десятков в нем.
+
+Входные данные
+Вводится двухзначное число.
+
+Выходные данные
+Выведите ответ на задачу.
+
+Примеры
+Входные данные
+42
+Выходные данные
+4
+         */
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        char output = input.charAt(input.length() - 2);
+        System.out.println(output);
+
+
+
+    }
+}
