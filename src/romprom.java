bc67ac4ee86d37bf7c87b883f9b147eafdbe76c5
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-05-10 22:25:18 -0400

    Add romprom.java

---
+++ src/romprom.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class romprom {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        // Гласные, которые мы хотим удалить
+        String vowels = "aeiouAEIOUаеёиоуыэюяАЕЁИОУЫЭЮЯ";
+
+        // Создаём строку без гласных
+        StringBuilder result = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            if (vowels.indexOf(c) == -1) {
+                result.append(c);
+            }
+        }
+
+        System.out.println(result.toString());
+    }
+}
