ae4b08429e5429d185ebe9895b8aad5c758bf011
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-05-13 18:33:23 -0400

    Add task 294.java

---
+++ src/task294.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class task294 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+
+        System.out.println(max);
+    }
+}
+
+//System.out.println(Math.max(a, Math.max(b, c)));
