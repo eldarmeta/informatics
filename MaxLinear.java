735fe285a8bac4cf827cf1a72b41f66b104e6c71
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-08-29 21:37:24 -0400

    Add max linear on java

---
+++ MaxLinear.java
@@ -0,0 +1,11 @@
+public class MaxLinear {
+    public int findMax(int[] nums) {
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        return max;
+    }
+}
