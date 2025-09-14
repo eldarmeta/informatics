e10d3ca7feb4c11a39bb9b1b4829abcd063660c3
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-09-13 21:53:30 -0400

    Add LC 704 binary search recursive on Java

---
+++ LC704_BinarySearchRecursive.java
@@ -0,0 +1,19 @@
+public class LC704_BinarySearchRecursive {
+    public static int search(int[] nums, int target) {
+        return helper(nums, target, 0, nums.length - 1);
+    }
+
+    private static int helper(int[] nums, int target, int left, int right) {
+        if (left > right) return -1;
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) return mid;
+        if (nums[mid] < target) return helper(nums, target, mid + 1, right);
+        return helper(nums, target, left, mid - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] a = {-1, 0, 3, 5, 9, 12};
+        System.out.println(search(a, 9));
+        System.out.println(search(a, 2));
+    }
+}
